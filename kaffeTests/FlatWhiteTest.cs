70fced3f37cebba5008a89572541ac97f77bfa4b
Anders Jagd Lauritsen <ande4845@edu.easj.dk>
2018-09-14 13:33:43 +0200

    tilføjet class og lavet test

---
+++ kaffeTests/FlatWhiteTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using kaffe;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace kaffeTests
+{
+    [TestClass]
+    public class FlatWhiteTest
+    {
+        [TestMethod]
+        public void FlatWhitePrisTest()
+        {
+            //Arrange
+            var flatwhite = new FlatWhite();
+
+            //Act
+            int prisFlatwhite = flatwhite.Pris();
+
+            //Assert
+            Assert.AreEqual(45, prisFlatwhite);
+        }
+
+        [TestMethod]
+        public void FlatWhiteStyrkeTest()
+        {
+            //Arrange
+            var flatwhite = new FlatWhite();
+
+            //Act
+            flatwhite.Styrke();
+        }
+
+        [TestMethod]
+        public void FlatWhiteMælkTest()
+        {
+            //Arrange
+            var flatwhite = new FlatWhite();
+
+            //Act
+            flatwhite.MlMælk();
+        }
+
+
+
+
+    }
+}
